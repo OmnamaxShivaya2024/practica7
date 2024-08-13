042cf2321013b0e636f6298a7844c1c2f6147653
Людмила Кузнецова <ananim2023@list.ru>
2024-08-13 13:16:36 +0300

    Add files via upload

--- 'module_1_6.py'.py
+++ 'module_1_6.py'.py
@@ -1,12 +1,19 @@
 my_dict={"Коля":987654321,"Петя":123456789}
 print(my_dict)
 print(my_dict["Коля"])
-my_dict.update({"Света":456789123,"Маша":123789456})
-del my_dict["Коля"]
+print(my_dict.get("Вика"))
+my_dict.update({"Света":456789789, "Катя":123123123})
+p=my_dict.pop("Петя")
+print(p)
 print(my_dict)
-my_set={1,1,2,2,3,3,"садик", "школа", "институт"}
+my_set={1,1,2,2,3,3,"садик", "школа","институт"}
 print(my_set)
-my_set.update({4,5})
-my_set.remove(1)
+my_set.update({"ясли",5})
 print(my_set)
+my_set.discard((1))
+print(my_set)
+
+
+
+
 
